1f7f02a7e6db3a62cf95f04ba1edd75bf9effd8e
wangyuebang <18061378605@163.com>
2020-08-15 11:17:03 +0800

    分页查询以及后台数据接入

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    data() {
+      const item = {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      };
+      return {
+        tableData: Array(20).fill(item)
+      }
+    }
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,60 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import PageOne from "../views/PageOne";
+import PageTwo from "../views/PageTwo";
+import App from "../App";
+import PageThree from "../views/PageThree";
+import PageFour from "../views/PageFour";
+import Index from "../views/Index";
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: '导航1',
+    component: Index,
+    redirect:'pageOne',
+    children :[
+      {
+        path: '/pageOne',
+        name: '页面1',
+        component: PageOne
+      },
+      {
+        path: '/pageTwo',
+        name: '页面2',
+        component: PageTwo
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+      path: '/navigation',
+      name: '导航2',
+      component: Index,
+      children :[
+        {
+          path: '/pageThree',
+          name: '页面3',
+          component: PageThree
+        },
+        {
+          path: '/pageFour',
+          name: '页面4',
+          component: PageFour
+        }
+      ]
+    }
+
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
